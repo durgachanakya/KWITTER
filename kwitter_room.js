6f68df55a7f7eed347e2d5db770c568b14773d4d
durgachanakya <81852237+durgachanakya@users.noreply.github.com>
2021-11-06 20:31:29 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,56 +1,18 @@
-
+// Import the functions you need from the SDKs you need
 //import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-      apiKey: "AIzaSyDSag0fGFm1QPhhUXZX9kpDFG8sXYlyG-g",
-      authDomain: "class-test-234ab.firebaseapp.com",
-      databaseURL: "https://class-test-234ab-default-rtdb.firebaseio.com",
-      projectId: "class-test-234ab",
-      storageBucket: "class-test-234ab.appspot.com",
-      messagingSenderId: "824046905779",
-      appId: "1:824046905779:web:2a520f486b26f95c0ac3c6"
-    };
-    
-    // Initialize Firebase
-    firebase.initializeApp(firebaseConfig);
-//ADD YOUR FIREBASE LINKS HERE
+  apiKey: "AIzaSyASFYzq7cB85fBubflsdIrrLhPGCWnT88s",
+  authDomain: "letschatapp-89335.firebaseapp.com",
+  databaseURL: "https://letschatapp-89335-default-rtdb.firebaseio.com",
+  projectId: "letschatapp-89335",
+  storageBucket: "letschatapp-89335.appspot.com",
+  messagingSenderId: "856994190131",
+  appId: "1:856994190131:web:b0b72f49e742c4c8042446"
+};
 
-user_name = localStorage.getItem("user_name");
-document.getElementById("user_name").innerHTML = "Welcome " + user_name + "!" ;
-
-function addRoom(){
-      room_name = document.getElementById("room_name").value;
-
-      firebase.database().ref("/").child(room_name).update({
-            purpose : "adding room name"
-      });
-
-      localStorage.setItem("room_name" , room_name);
-
-      window.location = "kwitter_page.html";
-}
-
-function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
-       Room_names = childKey;
-      //Start code
-      console.log("Room Name - " + Room_names);
-      row = "<div class = 'room_name' id = "+Room_names+" onclick = 'redirectToRoomName(this.id)'># "+Room_names+"</div><hr>";
-      document.getElementById("output").innerHTML += row ;
-      //End code
-      });});}
-getData();
-
-function redirectToRoomName(name){
-      console.log(name);
-      localStorage.setItem("room_name" , name);
-      window.location = "kwitter_page.html" ;
-}
-
-function logout(){
-      localStorage.removeItem("user_name");
-      localStorage.removeItem("room_name");
-      window.location = "index.html"
-}
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
